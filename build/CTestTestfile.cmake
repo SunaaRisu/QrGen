583571ab29fa52b0a05330ae028ad4df570d6b01
Sunaa <105628373+SunaaRisu@users.noreply.github.com>
2025-04-22 12:00:19 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sunaa/dev/qr
-# Build directory: /home/sunaa/dev/qr/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
